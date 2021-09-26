f7451e2639005f68b2f741fea472e9547f0bd4f6
Sekai <Felippe10_@hotmail.com>
2021-09-26 13:58:21 -0300

    refactor name class and layout

--- app/src/main/java/com/sekai/lisbeer/ui/user/UserFragment.kt
+++ app/src/main/java/com/sekai/lisbeer/ui/user/UserFragment.kt
@@ -1,4 +1,4 @@
-package com.sekai.lisbeer.ui.notifications
+package com.sekai.lisbeer.ui.user
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.sekai.lisbeer.R
-import com.sekai.lisbeer.databinding.FragmentNotificationsBinding
+import com.sekai.lisbeer.databinding.FragmentUserBinding
 
-class NotificationsFragment : Fragment() {
+class UserFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var userViewModel: UserViewModel
+    private var _binding: FragmentUserBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        userViewModel =
+            ViewModelProvider(this).get(UserViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        userViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/sekai/lisbeer/ui/user/UserViewModel.kt
+++ app/src/main/java/com/sekai/lisbeer/ui/user/UserViewModel.kt
@@ -1,13 +1,13 @@
-package com.sekai.lisbeer.ui.notifications
+package com.sekai.lisbeer.ui.user
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class UserViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is user Fragment"
     }
     val text: LiveData<String> = _text
 }
